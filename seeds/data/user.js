ce49fcf58a4d2aa1b2e3c4dc544b9bfcb9b88bb4
CahyaPurnama <IGedeCahyaPurnamaYasa@users.noreply.github.com>
2022-05-17 16:45:39 +0800

    update seed cashier

--- seeds/data/user.js
+++ seeds/data/user.js
@@ -6,4 +6,11 @@ module.exports = [
         roles : ['admin'],
         company_id : 'CD-0001'
     },
+    {
+        name: 'Cashier',
+        email: 'cashier@gmail.com',
+        password: 'admin123',
+        roles : ['cashier'],
+        company_id : 'CD-0001'
+    },
 ]
--- seeds/data/user_role.js
+++ seeds/data/user_role.js
@@ -2,5 +2,9 @@ module.exports = [
     {
         role_id: 'admin',
         user_email: 'administrator@gmail.com'
+    },
+    {
+        role_id: 'cashier',
+        user_email: 'cashier@gmail.com'
     }
 ];
